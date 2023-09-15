7ef762461bf0756c82a752e77d3832d6be0a117e
Leslie <Kooliesk@gmail.com>
2023-09-15 15:30:18 +0200

    fix linter errors

--- src/modules/navbar.js
+++ src/modules/navbar.js
@@ -7,12 +7,12 @@ export const nav = async () => {
   const counter = movieArray.length;
 
   // Generating number of moviez to navigation bar
-  const movvieItems = `<div class="col-2" id="logo" href="index.html"><img
+  const movvieItems = `<div class="col-2 mb-4" id="logo" href="index.html"><img
 src="https://png.pngtree.com/png-clipart/20200826/original/pngtree-movie-logo-movie-letter-v-png-image_5469427.jpg"
 alt="logo"></div>
-<div class="col-3"><a href="index.html">Moviez(${counter})</a></div>
-<div class="col-3">Planets</div>
-<div class="col-3">Races</div>`;
+<div class="col-3 mb-4"><a href="index.html">Moviez(${counter})</a></div>
+<div class="col-3 mb-4">Planets</div>
+<div class="col-3 mb-4">Races</div>`;
   navBar.innerHTML = movvieItems;
 };
 
